5814573c4ae12c451a4533f374343670d221b8c4
goranc89 <114668056+goranc89@users.noreply.github.com>
2022-11-01 20:35:12 +0100

    Update f9.js

--- js/function/function2/f9.js
+++ js/function/function2/f9.js
@@ -1,3 +1,6 @@
+
+//KOMENTAR: Svaka cast, uradila si na dva nacina.
+
 /*9. Write a function that replaces spaces in a string with provided separator. If separator is not
 provided, use “-” (dash) as the default separator.
 "My random string", "_" -> "My_random_string"
